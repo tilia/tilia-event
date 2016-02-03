65c1f239314f5e420a3ff26aef65815ceaef7ca2
Jakob Sack <mail@jakobsack.de>
2016-02-03 23:56:22 +0100

    Release v2.0.2.1

--- lib/tilia/event/version.rb
+++ lib/tilia/event/version.rb
@@ -3,7 +3,7 @@ module Tilia
     # This class contains the version number for this package.
     class Version
       # Full version number
-      VERSION = '2.0.2'.freeze
+      VERSION = '2.0.2.1'.freeze
     end
   end
 end
